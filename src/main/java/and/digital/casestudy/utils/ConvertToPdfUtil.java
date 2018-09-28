8edc8bdd0fbdf5721e3696c229e5acbcb6c94f47
Idrisha Das <idrishadas@Administrators-MacBook-Pro-2.local>
2018-09-28 12:27:51 +0100

    corrected PDF util

--- src/main/java/and/digital/casestudy/utils/ConvertToPdfUtil.java
+++ src/main/java/and/digital/casestudy/utils/ConvertToPdfUtil.java
@@ -2,7 +2,6 @@ package and.digital.casestudy.utils;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.itextpdf.text.*;
@@ -21,8 +20,8 @@ public class ConvertToPdfUtil {
 			PdfWriter.getInstance(pdfRecord, new FileOutputStream(home + "//Downloads//myPdf.pdf"));
 			pdfRecord.open();
 			report = new PdfPTable(5);
-			report.setWidthPercentage(100); 
-			report.setSpacingBefore(10f); 
+			report.setWidthPercentage(100);
+			report.setSpacingBefore(10f);
 			report.setSpacingAfter(10f);
 			PdfPCell cell;
 
@@ -37,43 +36,13 @@ public class ConvertToPdfUtil {
 				report.addCell(cell);
 				cell = new PdfPCell(new Phrase(cs.getTags()));
 				report.addCell(cell);
-				pdfRecord.add(report);
 			}
+			pdfRecord.add(report);
+
 		} catch (FileNotFoundException | DocumentException e) {
 			e.printStackTrace();
 		} finally {
 			pdfRecord.close();
 		}
-	}
-	
-	public static void main(String[] args) {
-		
-		List<CaseStudy> caseStudies = new ArrayList<CaseStudy>();
-		CaseStudy cs1= new CaseStudy();
-		cs1.setClientName("clientName1");
-		cs1.setName("name1");
-		cs1.setDescription("description1");
-		cs1.setSummary("summary1");
-		cs1.setTags("tags1");
-		
-		CaseStudy cs2= new CaseStudy();
-		cs2.setClientName("clientName1");
-		cs2.setName("name2");
-		cs2.setDescription("description1");
-		cs2.setSummary("summary1");
-		cs2.setTags("tags1");
-		
-		CaseStudy cs3= new CaseStudy();
-		cs3.setClientName("clientName3");
-		cs3.setName("name3");
-		cs3.setDescription("description3");
-		cs3.setSummary("summary3");
-		cs3.setTags("tags3");
-		
-		caseStudies.add(cs1);
-		caseStudies.add(cs2);
-		caseStudies.add(cs3);
-		convertToPdf(caseStudies);
-		
 	}
 }
