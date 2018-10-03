d6f97d08464e8d827355d6a6b9799bc2d774c0a1
idrishadas <idrishadas@192.168.67.28>
2018-10-03 22:04:09 +0100

    file stotage added

--- src/main/java/and/digital/casestudy/CaseStudyApplication.java
+++ src/main/java/and/digital/casestudy/CaseStudyApplication.java
@@ -3,9 +3,13 @@ package and.digital.casestudy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import com.amazonaws.services.databasemigrationservice.model.MongoDbSettings;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import and.digital.casestudy.models.FileStorageProperties;
 
 
 @SpringBootApplication
+@EnableConfigurationProperties({ FileStorageProperties.class })
 public class CaseStudyApplication {
 	
 	public static void main(String[] args) {
